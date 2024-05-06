45fd9bc4f5cebe1ca8e55b3269c2fc7b9774ad30
Petrônio Cândido de Lima e Silva <petronio.candido@gmail.com>
2024-05-06 20:32:34 -0300

    Migrating classifier to Transformer

--- models/classifier.py
+++ models/classifier.py
@@ -3,37 +3,33 @@ import torch
 from torch import nn
 
 from clshq_tk.modules.positional import PositionalEncoder
-from clshq_tk.modules.attention import MultiHeadAttention
+from clshq_tk.modules.transformer import Transformer
 
 class TSAttentionClassifier(nn.Module):
-  def __init__(self, tokenizer, num_tokens, device = None, **kwargs):
+  def __init__(self, tokenizer, num_tokens, num_layers, device = None, **kwargs):
     super().__init__()
 
     self.num_tokens = num_tokens
+    self.num_layers = num_layers
     self.tokenizer = tokenizer
     self.tokenizer.freeze()
     self.device = device
     self.positional = PositionalEncoder(self.num_tokens, self.tokenizer.embed_dim)
-    self.attention = MultiHeadAttention(self.tokenizer.num_classes, self.num_tokens, self.tokenizer.embed_dim)
-    self.ln = nn.LayerNorm(self.tokenizer.embed_dim)
+    self.transformers = [Transformer(self.tokenizer.num_classes, self.num_tokens, self.tokenizer.embed_dim, self.num_tokens * self.tokenizer.num_classes) 
+                         for k in range(num_layers)]
     self.flat = nn.Flatten(1)
-    self.linear1 = nn.Linear(self.num_tokens * self.tokenizer.embed_dim, self.num_tokens * self.tokenizer.num_classes)
-    self.linear2 = nn.Linear(self.num_tokens * self.tokenizer.num_classes, self.num_tokens * self.tokenizer.num_classes)
-    self.linear3 = nn.Linear(self.num_tokens * self.tokenizer.num_classes, self.tokenizer.num_classes)
-    self.gelu = nn.GELU()
+    self.linear = nn.Linear(self.num_tokens * self.tokenizer.embed_dim, self.tokenizer.num_classes)
     self.relu = nn.ReLU()
     self.drop = nn.Dropout(.25)
     self.sm = nn.LogSoftmax(dim=1)
 
   def forward(self, x):
     tokens = self.tokenizer(x)
-    tokens = self.positional(tokens)
-    z = self.attention(tokens)
-    z = self.ln(tokens + z)
+    z = self.positional(tokens)
+    for k in range(self.num_layers):
+      z = self.transformers[k](z)
     z = self.flat(z)
-    z = self.gelu(self.linear1(self.drop(z)))
-    z = self.gelu(self.linear2(self.drop(z)))
-    z = self.relu(self.linear3(z))
+    z = self.relu(self.linear(z))
     z = self.sm(z)
     return z
 
@@ -46,16 +42,16 @@ class TSAttentionClassifier(nn.Module):
     self.device = args[0]
     self.tokenizer = self.tokenizer.to(self.device)
     self.positional = self.positional.to(self.device)
-    self.attention = self.attention.to(self.device)
-    self.linear1 = self.linear1.to(self.device)
-    self.linear2 = self.linear2.to(self.device)
+    for k in range(self.num_layers):
+      self.transformers[k] = self.transformers[k].to(self.device)
+    self.linear = self.linear.to(self.device)
     return self
 
   def train(self, *args, **kwargs):
     super().train(*args, **kwargs)
     self.tokenizer = self.tokenizer.train(*args, **kwargs)
     self.positional = self.positional.train(*args, **kwargs)
-    self.attention = self.attention.train(*args, **kwargs)
-    self.linear1 = self.linear1.train(*args, **kwargs)
-    self.linear2 = self.linear2.train(*args, **kwargs)
+    for k in range(self.num_layers):
+      self.transformers[k] = self.transformers[k].train(*args, **kwargs)
+    self.linear = self.linear.train(*args, **kwargs)
     return self
